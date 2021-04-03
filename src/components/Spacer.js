6f5ab854016cf58884333c8eba75562ec4fdcfaa
Jen Speak <jspeak6@gmail.com>
2021-04-03 14:35:27 -0700

    add function to Spacer to make more DRY

--- src/components/Spacer.js
+++ src/components/Spacer.js
@@ -1,68 +1,55 @@
-// Borrowed from https://www.joshwcomeau.com/react/modern-spacer-gif/
+// Borrowed and tweaked from https://www.joshwcomeau.com/react/modern-spacer-gif/
 // Used to create space between components
 
 import styled from 'styled-components/macro';
 
 import { BREAKPOINTS } from '../constants';
 
+function getResponsiveSpace(breakpoint) {
+	return typeof breakpoint === 'number' ? breakpoint : undefined;
+}
+
 const Spacer = styled.span`
 	display: block;
-	width: ${p => p.size + 'px'};
-	min-width: ${p => p.size + 'px'};
-	height: ${p => p.size + 'px'};
-	min-height: ${p => p.size + 'px'};
+	width: ${p => p.size}px;
+	min-width: ${p => p.size}px;
+	height: ${p => p.size}px;
+	min-height: ${p => p.size}px;
 
 	// Make Spacer responsive
 	@media ${BREAKPOINTS.mobileSmall} {
-		width: ${p =>
-			p.mobileSmallAndUp ? p.mobileSmallAndUp + 'px' : undefined};
-		min-width: ${p =>
-			p.mobileSmallAndUp ? p.mobileSmallAndUp + 'px' : undefined};
-		height: ${p =>
-			p.mobileSmallAndUp ? p.mobileSmallAndUp + 'px' : undefined};
-		min-height: ${p =>
-			p.mobileSmallAndUp ? p.mobileSmallAndUp + 'px' : undefined};
+		width: ${p => getResponsiveSpace(p.mobileSmallAndUp)}px;
+		min-width: ${p => getResponsiveSpace(p.mobileSmallAndUp)}px;
+		height: ${p => getResponsiveSpace(p.mobileSmallAndUp)}px;
+		min-height: ${p => getResponsiveSpace(p.mobileSmallAndUp)}px;
 	}
 
 	@media ${BREAKPOINTS.mobile} {
-		width: ${p => (p.mobileAndUp ? p.mobileAndUp + 'px' : undefined)};
-		min-width: ${p => (p.mobileAndUp ? p.mobileAndUp + 'px' : undefined)};
-		height: ${p => (p.mobileAndUp ? p.mobileAndUp + 'px' : undefined)};
-		min-height: ${p => (p.mobileAndUp ? p.mobileAndUp + 'px' : undefined)};
+		width: ${p => getResponsiveSpace(p.mobileAndUp)}px;
+		min-width: ${p => getResponsiveSpace(p.mobileAndUp)}px;
+		height: ${p => getResponsiveSpace(p.mobileAndUp)}px;
+		min-height: ${p => getResponsiveSpace(p.mobileAndUp)}px;
 	}
 
 	@media ${BREAKPOINTS.tablet} {
-		width: ${p => (p.tabletAndUp ? p.tabletAndUp + 'px' : undefined)};
-		min-width: ${p => (p.tabletAndUp ? p.tabletAndUp + 'px' : undefined)};
-		height: ${p => (p.tabletAndUp ? p.tabletAndUp + 'px' : undefined)};
-		min-height: ${p => (p.tabletAndUp ? p.tabletAndUp + 'px' : undefined)};
+		width: ${p => getResponsiveSpace(p.tabletAndUp)}px;
+		min-width: ${p => getResponsiveSpace(p.tabletAndUp)}px;
+		height: ${p => getResponsiveSpace(p.tabletAndUp)}px;
+		min-height: ${p => getResponsiveSpace(p.tabletAndUp)}px;
 	}
 
 	@media ${BREAKPOINTS.laptop} {
-		width: ${p =>
-			typeof p.laptopAndUp === 'number'
-				? p.laptopAndUp + 'px'
-				: undefined};
-		min-width: ${p =>
-			typeof p.laptopAndUp === 'number'
-				? p.laptopAndUp + 'px'
-				: undefined};
-		height: ${p =>
-			typeof p.laptopAndUp === 'number'
-				? p.laptopAndUp + 'px'
-				: undefined};
-		min-height: ${p =>
-			typeof p.laptopAndUp === 'number'
-				? p.laptopAndUp + 'px'
-				: undefined};
+		width: ${p => getResponsiveSpace(p.laptopAndUp)}px;
+		min-width: ${p => getResponsiveSpace(p.laptopAndUp)}px;
+		height: ${p => getResponsiveSpace(p.laptopAndUp)}px;
+		min-height: ${p => getResponsiveSpace(p.laptopAndUp)}px;
 	}
 
 	@media ${BREAKPOINTS.desktop} {
-		width: ${p => (p.desktopAndUp ? p.desktopAndUp + 'px' : undefined)};
-		min-width: ${p => (p.desktopAndUp ? p.desktopAndUp + 'px' : undefined)};
-		height: ${p => (p.desktopAndUp ? p.desktopAndUp + 'px' : undefined)};
-		min-height: ${p =>
-			p.desktopAndUp ? p.desktopAndUp + 'px' : undefined};
+		width: ${p => getResponsiveSpace(p.desktopAndUp)}px;
+		min-width: ${p => getResponsiveSpace(p.desktopAndUp)}px;
+		height: ${p => getResponsiveSpace(p.desktopAndUp)}px;
+		min-height: ${p => getResponsiveSpace(p.desktopAndUp)}px;
 	}
 `;
 
